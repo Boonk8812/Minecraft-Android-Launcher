0fb4dd970719b60c8e9e79bfc733d0eba7d9e5b7
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-09-15 11:37:03 -0700

    Create app.java

---
+++ app.java
@@ -0,0 +1,16 @@
+var filePath(580000100010);
+{
+
+// If Minecraft allocates too much memory, close Minecraft.
+
+arm <- supported::os/ "Android 14 - Android 1.0"
+  
+class CPU_SUPPORT("any // CPUs from 2008 to 2023");
+
+; 00 FF # ldi 00 F4
+; 00 FF # jmp 00 FF
+; 00 FF # jmp 00 FF
+
+get::version("v0.0.0 build 0.0");
+  
+}
