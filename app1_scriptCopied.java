ffe12d0a43d4260349e26b253c448a11948e0fc6
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-09-24 19:27:39 -0700

    Create app1_scriptCopied.java

---
+++ app1_scriptCopied.java
@@ -0,0 +1,87 @@
+function var filePath(580000100010);
+{
+
+// If Minecraft allocates too much memory, close Minecraft.
+
+arm <- supported::os/ "Android 14 - Android 1.0"
+  
+class CPU_SUPPORT("any // CPUs from 2008 to 2023");
+
+; 00 FF # ldi 00 F4
+; 00 FF # jmp 00 FF
+; 00 FF # jmp 00 FF
+
+get::version("v0.0.0 build 0.0");
+  
+}
+do   System.out.printIn("▌▌▌▌▌▌▌▌▌Hello!▌▌▌▌▌▌▌▌▌");
+
+
+
+
+if ("MC_LAUNCHER_v0.0.0_build_0.0.jar".equals(System.getProperty("program.name"))) {
+   // perform appropriate actions...
+}
+@Value("${info.app.version:0.0.0 build 0.0 (not beta)}") String version
+{
+#include  <\.github>
+  #include  <\.worlds>
+   #include  <\main>
+    #include  <\.download>
+     #include  <\.textures>
+      #include  <\.download>
+
+  }
+public class VersionHolder {
+
+    private final String version;
+
+    public VersionHolder(ApplicationContext context) {
+        version = context.getBeansWithAnnotation(SpringBootApplication.class).entrySet().stream()
+                .findFirst()
+                .flatMap(es -> {
+                    final String implementationVersion = es.getValue().getClass().getPackage().getImplementationVersion();
+                    return Optional.ofNullable(implementationVersion);
+                }).orElse("unknown");
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
+
+@Configuration
+public class MyConfig{
+
+    @Bean
+    VersionHolder getVersionHolder(ApplicationContext context){
+        return new VersionHolder(context);
+    }
+}
+
+
+
+
+}
+public class PlayerInventoryCount
+
+@PlayerInvCnt }
+std::countPlayerInventory
+
+; 
+return PlayerInventoryCount
+}
+new PlayerInventoryCount ; public private Class_ String_HIMEM;
+
+  // If player tries to get their player count bigger then 9000x items, then close Minecraft with exit code java.lang.Execption.PlayerInvItemCount.IllegalCountValue
+
+
+rnd(* 9000 + 10.1 = 1.0 , 0.1 = Close ; -0.0 = Nothing);
+
+public class getClass 0x0000000 }.typeof($this 0x00 #pragma ;}
+
+
+}. 0x0000000000000 @ jmp 0x0= typeof; == $Component:="getClassFunctionHandler");
+}. 0x0000000000A77 @ jmp 0x0= typeof; == $Component:="getClassFunctionHandler");
+
+-0x00000A7, }. 0x0000000, $000, .=}. 0x000000000FFFFFFF3AC66
