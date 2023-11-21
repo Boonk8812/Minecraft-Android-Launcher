1ea1ab0506069f6da3bc05e494fa8b86ec101ba1
Boonk3312 <theboonk7@hotmail.com>
2023-11-21 13:04:33 -0800

    Update app1_scriptCopied.java

--- app1_scriptCopied.java
+++ app1_scriptCopied.java
@@ -85,3 +85,144 @@ public class getClass 0x0000000 }.typeof($this 0x00 #pragma ;}
 }. 0x0000000000A77 @ jmp 0x0= typeof; == $Component:="getClassFunctionHandler");
 
 -0x00000A7, }. 0x0000000, $000, .=}. 0x000000000FFFFFFF3AC66
+
+  gcc(compile):
+
+  NoAI(disabled);
+
+private final class BottomCode {
+  
+ import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ApkZipExecutor {
+    
+    public void executeApk(String apkFilePath) {
+        try {
+            // Check if the file exists
+            File apkFile = new File(apkFilePath);
+            if (!apkFile.exists()) {
+                throw new IllegalArgumentException("Apk file does not exist.");
+            }
+            
+        import java.io.File;
+import java.io.IOException;
+
+public class ApkExecutor {
+    public static void executeApk(String apkFilePath) {
+        try {
+            // Check if the APK file exists
+            File apkFile = new File(apkFilePath);
+            if (!apkFile.exists()) {
+                throw new IllegalArgumentException("APK file does not exist");
+            }
+
+            // Execute the APK file
+            String command = "adb install -r " + apkFilePath;
+            Process process = Runtime.getRuntime().exec(command);
+
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+
+            // Check the exit code to determine if the execution was successful
+            if (exitCode == 0) {
+                System.out.println("APK execution successful");
+            } else {
+                System.out.println("APK execution failed");
+            }
+        } catch (IOException e) {
+            System.out.println("Error executing APK: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println("APK execution interrupted: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+
+      import java.io.File;
+
+public class APKDirectory {
+    public static void main(String[] args) {
+        String directoryPath = (specifiedFilePath);
+        String apkFileName = (specifiedFileName);
+
+        File apkFile = new File(directoryPath, apkFileName);
+
+        if (apkFile.exists()) {
+            System.out.println("APK file found at: " + apkFile.getAbsolutePath());
+        } else {
+            System.out.println("APK file not found.");
+        }
+    }
+}
+
+        String apkFilePath = (apkFilePathSpecified);
+        executeApk(apkFilePath);
+    }
+}
+
+            
+        } catch (Exception e) {
+            // Handle any exceptions that occur during execution
+            System.err.println("Error executing apk: " + e.getMessage());
+        }
+    }
+    
+    public void executeZip(String zipFilePath) {
+        try {
+            // Check if the file exists
+            File zipFile = new File(zipFilePath);
+            if (!zipFile.exists()) {
+                throw new IllegalArgumentException("Zip file does not exist.");
+            }
+
+          import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipFile;
+
+public class ZipExecutor {
+    public void executeFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            
+            if (!file.exists()) {
+                throw new IllegalArgumentException("File does not exist!");
+            }
+            
+            if (file.getName().endsWith(".apk")) {
+                // Execute APK file
+                executeApk(file);
+            } else if (file.getName().endsWith(".zip")) {
+                // Execute ZIP file
+                executeZip(file);
+            } else {
+                throw new IllegalArgumentException("Invalid file format!");
+            }
+        } catch (IOException e) {
+            System.err.println("Error executing file: " + e.getMessage());
+        }
+    }
+    
+    private void executeApk(File apkFile) throws IOException {
+        // Code to execute APK file
+        // ...
+    }
+    
+    private void executeZip(File zipFile) throws IOException {
+        // Code to execute ZIP file
+        // ...
+    }
+}
+
+            
+        } catch (Exception e) {
+            // Handle any exceptions that occur during execution
+            System.err.println("Error executing zip: " + e.getMessage());
+        }
+    }
+}
+ 
+}
